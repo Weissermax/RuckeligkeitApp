2cecc2fa8a7232d8f7bdaa8ca58c5320e8e4eb1b
Weissermax <it13037@lehre.dhbw-stuttgart.de>
2015-11-19 09:57:42 +0100

    Added new page 'records'

--- TestApp/app/src/main/java/com/example/mm_000/testapp/RecordsActivity.java
+++ TestApp/app/src/main/java/com/example/mm_000/testapp/RecordsActivity.java
@@ -7,53 +7,30 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.widget.TextView;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class RecordsActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
-    private TextView xValueText, yValueText, zValueText;
-    private Sensor mySensor;
-    private SensorManager mySensorManager;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_records);
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
-        // Create Sensor Manager
-        mySensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
 
-        // Accelerometer Sensor
-        mySensor = mySensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        // Register sensor Listener
-        mySensorManager.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
-        //Try also the other DELAY-Sensors:
-        //mySensorManager.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_GAME);
-        //mySensorManager.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_FASTEST);
-
-        xValueText = (TextView)findViewById(R.id.xValueText);
-        yValueText = (TextView)findViewById(R.id.yValueText);
-        zValueText = (TextView)findViewById(R.id.zValueText);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_records, menu);
         return true;
     }
 
@@ -77,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         if(id == R.id.action_records)
         {
-            startActivity(new Intent(MainActivity.this, RecordsActivity.class));
+            startActivity(new Intent(RecordsActivity.this, RecordsActivity.class));
         }
 
         if(id == R.id.action_export)
@@ -103,15 +80,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        xValueText.setText("X: "+ event.values[0]);
-        yValueText.setText("Y: "+ event.values[1]);
-        zValueText.setText("Z: "+ event.values[2]);
-    }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Not in use
-    }
 }
